433b996513794ba6d704b1e96982188af691f79a
Jungwoo Kim <rlawjddn0623@gmail.com>
2024-08-04 15:05:02 +0900

    Time: 111 ms (32.48%), Space: 20.1 MB (32.19%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -25,7 +25,7 @@ class Solution:
                     dp[i][j] = dp[i-1][j-1]
                 else:
                     dp[i][j] = min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j]) + 1
-        print(dp)
+                    
         return dp[-1][-1]
                 
 
