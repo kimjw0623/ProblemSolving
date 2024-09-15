e652d35753334b26a88cfd1ee96d015f19fe8405
Jungwoo Kim <rlawjddn0623@gmail.com>
2024-09-15 22:40:47 +0900

    Time: 87 ms (80.26%), Space: 16.6 MB (94.43%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,37 +1,21 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        # DP
-        if not word1:
-            return len(word2)
-        if not word2:
-            return len(word1)
         l1,l2 = len(word1),len(word2)
-        dp = [[-1 for _ in range(l2)] for _ in range(l1)]
-        for i in range(l1):
-            for j in range(l2):
-                if i == 0 and j == 0:
-                    if word1[i] == word2[j]:
-                        dp[0][0] = 0
-                    else:
-                        dp[0][0] = 1
-                    continue
-                if i == 0:
-                    if word1[i] == word2[j] and dp[0][j-1] > j-1:
-                        dp[0][j] = dp[0][j-1]
-                    else:
-                        dp[0][j] = dp[0][j-1] + 1 # insert char
-                    continue
-                if j == 0:
-                    if word1[i] == word2[j] and dp[i-1][0] > i-1:
-                        dp[i][j] = dp[i-1][j]
-                    else:
-                        dp[i][j] = dp[i-1][j] + 1 # ins
-                    continue
-                if word1[i] == word2[j]:
-                    dp[i][j] = dp[i-1][j-1]
+        dp = [0 for _ in range(l2+1)]
+        temp = [0 for _ in range(l2+1)]
+        for i in range(l2+1):
+            dp[i] = i
+
+        for i in range(1,l1+1):
+            dp[0] = i
+            temp[0] = i-1
+            for j in range(1,l2+1):
+                temp[j] = dp[j]
+                if word1[i-1] == word2[j-1]:
+                    dp[j] = temp[j-1]
                 else:
-                    dp[i][j] = min(dp[i-1][j-1],dp[i-1][j],dp[i][j-1])+1
-        
-        return dp[-1][-1]
+                    dp[j] = min(temp[j-1],dp[j-1],dp[j]) + 1
+
+        return dp[-1]
                 
-                
+
