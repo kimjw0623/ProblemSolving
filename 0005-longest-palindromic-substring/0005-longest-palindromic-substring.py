6e609007f8286e3a5e61c3d8e68ed0824e0767f9
Jungwoo Kim <rlawjddn0623@gmail.com>
2024-09-15 20:59:23 +0900

    Time: 97 ms (96.65%), Space: 14 MB (100%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 0:
+            return ""
+        start = 0
+        maxlen = 0
+        for i in range(len(s)):
+            if i-maxlen>=1 and s[i-maxlen-1:i+1] == s[i-maxlen-1:i+1][::-1]:
+                start = i-maxlen-1
+                maxlen += 2
+            elif i-maxlen>=0 and s[i-maxlen:i+1] == s[i-maxlen:i+1][::-1]:
+                start = i-maxlen
+                maxlen += 1
+            #print(start,maxlen)
+        return s[start:start+maxlen]
+                
+        
