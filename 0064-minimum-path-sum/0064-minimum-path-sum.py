1760e7219101e05f72eb4145dd06f98e633df240
Jungwoo Kim <rlawjddn0623@gmail.com>
2024-09-15 22:14:35 +0900

    Time: 75 ms (96.06%), Space: 18.4 MB (56.27%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,20 +1,36 @@
-
+from collections import deque
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        h,w = len(grid),len(grid[0])
-        dp = [[-1 for i in range(w)] for _ in range(h)]
+        # (m+n)!/m!*n!
+        # que = deque()
+        # h,w = len(grid),len(grid[0])
+        # visited = [[False for _ in range(w)] for _ in range(h)]
+        # mv = [(1,0),(0,1)]
+        # que.append([0,0,grid[0][0]])
+        # ans_list = []
+        # while que:
+        #     cur = que.popleft()
+        #     if cur[1] == h-1 and cur[0] == w-1:
+        #         ans_list.append(cur[2])
+        #     for mx,my in mv:
+        #         nx = mx+cur[0]
+        #         ny = my+cur[1]
+        #         if nx < w and ny < h:
+        #             que.append([nx,ny,cur[2]+grid[ny][nx]])
         
-        for y in range(h):
-            for x in range(w):
-                if y == 0 and x == 0: 
-                    dp[0][0] = grid[0][0]
-                    continue
-                upper_val = 9999999
-                left_val = 9999999
-                if y != 0:
-                    upper_val = dp[y-1][x]
-                if x != 0:
-                    left_val = dp[y][x-1]
-                dp[y][x] = min(upper_val,left_val) + grid[y][x]
-        print(dp[-1])
+        # return min(ans_list)
+
+        h,w = len(grid),len(grid[0])
+        dp = [[-1 for _ in range(w)] for _ in range(h)]
+
+        dp[0][0] = grid[0][0]
+        for x in range(1,w):
+            dp[0][x] = dp[0][x-1] + grid[0][x]
+        for y in range(1,h):
+            dp[y][0] = dp[y-1][0] + grid[y][0]
+
+        for y in range(1,h):
+            for x in range(1,w):
+                dp[y][x] = min(dp[y-1][x],dp[y][x-1])+grid[y][x]
+        
         return dp[-1][-1]
