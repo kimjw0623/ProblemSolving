f1f97c27ff95aeba7be6a7004ea4c24767e2dfe6
Jungwoo Kim <rlawjddn0623@gmail.com>
2024-09-16 22:04:51 +0900

    Time: 617 ms (55.31%), Space: 157.2 MB (11.64%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,25 +1,14 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # sorting first
-        # make sum(nums)//2
-        
-        # if sum(nums) % 2 == 1:
-        #     return False
-        # half = sum(nums) // 2
-        # @cache
-        # def rec(s,i):
-        #     if i > len(nums)-1 or s < 1:
-        #         return False
-        #     if s == nums[i]:
-        #         return True
-        #     return rec(s,i+1) or rec(s-nums[i],i+1)
-        # return rec(half,0)
-        total_sum = sum(nums)
-        if total_sum & 1: return False
-        half = total_sum // 2
-        dp = [True] + [False]*half
-        for num in nums:
-            for j in range(half, 0,-1):
-                if j-num >= 0 and dp[j-num]: dp[j] = True
-        return dp[half] 
-        
+        # 0/1 knapsack problem
+        if sum(nums)%2: return False
+        target_sum = sum(nums)//2
+        # recursive
+        @cache
+        def recursive(target,idx):
+            if target == 0:
+                return True
+            if idx >= len(nums) or target < 0:
+                return False
+            return recursive(target-nums[idx],idx+1) or recursive(target,idx+1)
+        return recursive(target_sum,0)
